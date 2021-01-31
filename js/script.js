a8526c4063861dee641f46bdeaf88b876ab471f3
Civodull <olivierndi57@gmail.com>
2021-01-31 00:30:33 +0000

    Modification heure et date

--- js/script.js
+++ js/script.js
@@ -63,41 +63,65 @@ function etatButton(valeurRecu) {
     //il faut rajouter la balise img et recuperer son id
     document.getElementById('img').src = etatImage;
 }
+
 var socket = io();
 socket.on('temp', function(data) {
     console.log(data);
-    document.getElementById('zone1Tem').innerHTML = `${data}`;
-    var teperature = `${data}`;
+    var temperature = data.slice(0, 2);
+    var humidite = data.slice(5, 7);
+    document.getElementById('tempAct').innerHTML = `${temperature}`;
+    document.getElementById('HumAct').innerHTML = `${humidite}`;
+
+    // var datHeure = new Date();
+    // var heur = datHeure.getHours();
+    // var min = datHeure.getMinutes();
+
+    // var numMois = datHeure.getMonth() + 1;
+    // if (numMois < 10) {
+    //     numMois = '0' + numMois;
+    // }
+    // var jour = datHeure.getDay(); //le numero du jour de la semaine
+    // var seco = datHeure.getSeconds(); //seconde
+    // var laDate = datHeure.getFullYear(); // me renvoie en chiffre l'annee
+    // var mois = datHeure.getDate(); //renvoie le jour du mois 
+    // var data = datHeure.getMonth(); //renvoie le jour du mois
+    // var heureEtDate = mois + '/' + numMois + '/' + laDate;
+    // document.getElementById('date').innerHTML = heureEtDate;
+
     //fonction pour changer d'images en fonction de la temperature
     //document.getElementById('img').src = 'im1';
-    if (data <= 26) {
-        document.getElementById('img').setAttribute('src', 'im1');
+    if (temperature <= 26) {
+        document.getElementById('img').innerHTML = '<img src="/im1" alt="Ambient" title="Temperature < = 26 " style="width:40px; height:44px;">';
     } else {
-        document.getElementById('img').setAttribute('src', 'imLogo');
+        document.getElementById('img').innerHTML = '<img src="/imLogo" alt="Chaud" title="Temperature >= 26 " style="width:40px; height:44px; border-radius: 32px;">';
     }
-    if (data <= 26) {
-        document.getElementById('rectangle2').innerHTML = '<img src="/im" alt="Il fait beau" style="width:640px; height:544px; float:left;">';
+    if (temperature <= 26) {
+        document.getElementById('rectangle2').innerHTML = '<img src="/im" alt="Il fait beau" style="width:auto; height:auto; float:left;">';
     } else {
-        document.getElementById('rectangle2').innerHTML = '<img src="/im2" alt="Il fait chaud" style="width:640px; height:544px; float:left;">';
+        document.getElementById('rectangle2').innerHTML = '<img src="/im2" alt="Il fait chaud" style="width:auto; height:auto; float:left;">';
+    }
+    date = new Date();
+    mois = date.getMonth();
+    if (mois == (mois + 1) || mois == (mois + 2) || mois == (mois + 6) || mois == (mois + 8) || mois == (mois + 12)) {
+        document.getElementById('saisonn').innerHTML = "E T E";
+    }
+    if (mois == (mois + 3) || mois == (mois + 4) || mois == (mois + 5) || mois == (mois + 7) || mois == (mois + 9) || mois == (mois + 10) || mois == (mois + 11)) {
+        document.getElementById('saisonn').innerHTML = "H I V E R";
     }
 });
 
-function date_heure(id) {
-    date = new Date;
-    annee = date.getFullYear();
-    moi = date.getMonth();
-    mois = new Array('Janvier', 'F&eacute;vrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Ao&ucirc;t', 'Septembre', 'Octobre', 'Novembre', 'D&eacute;cembre');
-    j = date.getDate();
-    jour = date.getDay();
-    jours = new Array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
-    h = date.getHours();
-    if (h < 10) { h = "0" + h; }
-    m = date.getMinutes();
-    if (m < 10) { m = "0" + m; }
-    s = date.getSeconds();
-    if (s < 10) { s = "0" + s; }
-    resultat = '' + jours[jour] + ' ' + j + ' ' + mois[moi] + ' ' + annee + ' ' + h + ':' + m + ':' + s;
-    document.getElementById(id).innerHTML = resultat;
-    setTimeout('date_heure("' + id + '");', '1000');
-    return true;
+function Heure() {
+    jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
+    mois = new Array("janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre");
+    d = new Date;
+    if (d.getHours() < 10) { h = "0" + d.getHours() } else { h = d.getHours() }
+    if (d.getMinutes() < 10) { min = "0" + d.getMinutes() } else { min = d.getMinutes() }
+    if (d.getSeconds() < 10) { sec = "0" + d.getSeconds() } else { sec = d.getSeconds() }
+    j = jours[d.getDay()];
+    if (d.getDate() < 10) { n = "0" + d.getDate() } else { n = d.getDate() }
+    m = mois[d.getMonth()];
+    a = d.getYear();
+    txt = "Nous sommes le " + j + " " + n + " " + m + " " + a + ", il est : " + h + ":" + min + ":" + sec;
+    Calk.innerHTML = txt;
+    setTimeout("Heure()", 1000);
 }
